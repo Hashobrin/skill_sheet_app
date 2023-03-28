edf4ac2cbb795143dd467a88d23c00379869b275
Fubii <hashobrin@gmail.com>
2023-03-28 02:20:19 +0000

    cron push

--- app/app.py
+++ app/app.py
@@ -56,9 +56,6 @@ class Skill(db.Model):
     level = db.Column(Integer)
 
 
-db.create_all()
-
-
 class LoginForm(FlaskForm):
     email = StringField('email')
     password = PasswordField('password')
@@ -116,6 +113,11 @@ class ChangePassword(FlaskForm):
         if new_password.data in ('/', '*', '\\'):
             raise ValidationError('"/", "*" and "\\" are prohibited charactors.')
 
+
+@app.before_first_request
+def init():
+    db.create_all()
+    
 
 @login_manager.user_loader
 def load_user(id):
